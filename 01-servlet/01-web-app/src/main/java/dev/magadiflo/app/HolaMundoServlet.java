bd030f9907aba611fce966740e3f4f3bb2802c56
Martín Díaz <magadiflo@gmail.com>
2026-03-09 00:06:01 -0500

    Primer Ejemplo con Servlet

---
+++ 01-servlet/01-web-app/src/main/java/dev/magadiflo/app/HolaMundoServlet.java
@@ -0,0 +1,36 @@
+package dev.magadiflo.app;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hola-mundo")
+public class HolaMundoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("""
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <h1>Primer ejemplo con Servlet</h1>
+                    <p>Mi primer app con Servlet y Jakarta EE 11</p>
+                </body>
+                </html>
+                """);
+        out.close();
+    }
+}
